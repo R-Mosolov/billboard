b93978d06afa98c186cd21c7de648b92a49731d7
R-Mosolov <r.v.mosolov@ya.ru>
2019-09-25 12:09:45 +0300

    Added separate field for validation requirements
    Also improved price validation.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // Global Variables
 const submit = document.querySelector('.form__submit');
-let requirements = [];
+const requirementsField = document.createElement('p');
+let requirementsList = [];
 
 // Form Activation
 const formActivation = document.querySelector('.map__pin');
@@ -15,6 +16,7 @@ submit.addEventListener('click', function (event) {
 
     validateTitle();
     validatePrice();
+    createRecommendations();
 });
 
 // Additional Functions for Form Validation
@@ -22,15 +24,15 @@ function validateTitle() {
     const lengthTitleValue = document.querySelector('#title').value.length;
 
     if (lengthTitleValue === 0) {
-        requirements.push('Пожалуйста, заполните поле: Заголовок объявления');
+        requirementsList.push('Пожалуйста, заполните поле: Заголовок объявления');
     }
 
     if (lengthTitleValue !== 0 && lengthTitleValue < 30) {
-        requirements.push('Минимальная длина поля "Заголовок объявления": 30 символов');
+        requirementsList.push('Минимальная длина поля "Заголовок объявления": 30 символов');
     }
 
     if (lengthTitleValue > 100) {
-        requirements.push('Максимальная длина поля "Заголовок объявления": 100 символов');
+        requirementsList.push('Максимальная длина поля "Заголовок объявления": 100 символов');
     }
 }
 
@@ -41,22 +43,38 @@ function validatePrice() {
     const minPrice = [0, 1000, 5000, 10000];
 
     if (lengthPriceValue === 0) {
-        requirements.push(' Пожалуйста, заполните поле: Цена за ночь, руб.');
+        requirementsList.push(' Пожалуйста, заполните поле: Цена за ночь, руб.');
     }
 
-    const getMinPrice = (type, index) => {
-        if (typeValue === `${type}`) {
-            document.querySelector('#price').placeholder = minPrice[index];
-            requirements.push(` Цена должна быть не меньше ${minPrice[index]} руб.`);
+    else {
+        const getMinPrice = (type, index) => {
+            if (typeValue === `${type}`) {
+                document.querySelector('#price').placeholder = minPrice[index];
+
+                if (priceValue < minPrice[index]) {
+                    requirementsList.push(` Цена должна быть не меньше ${minPrice[index]} руб.`);
+                }
+            }
+        };
+
+        getMinPrice('bungalo', 0);
+        getMinPrice('flat', 1);
+        getMinPrice('house', 2);
+        getMinPrice('palace', 3);
+
+        if (parseInt(priceValue, 10) > 1000000) {
+            requirementsList.push(' Цена не должна быть больше 1000000 руб.');
         }
-    };
+    }
+}
 
-    getMinPrice('bungalo', 0);
-    getMinPrice('flat', 1);
-    getMinPrice('house', 2);
-    getMinPrice('palace', 3);
+function createRecommendations() {
+    if (requirementsList.length > 0) {
+        const submitForm = document.querySelector('.form__element--submit');
 
-    if (parseInt(priceValue, 10) > 1000000) {
-        requirements.push(' Цена не должна быть больше 1000000 руб.');
+        requirementsField.innerHTML = requirementsList;
+        submitForm.appendChild(requirementsField);
+
+        requirementsList = [];
     }
 }
